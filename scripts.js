134c8e1f4206d059190132c668f12caade12b04d
Julian Pinilla <julesp97@gmail.com>
2025-03-10 20:50:09 -0500

    funcion de ronda

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
 function getRandomComputerResult() {
-    const options = ["Rock", "Paper", "Scissors"];
-    const randomIndex = Math.floor(Math.random * options.length);
-    return options[randomIndex];
+  const options = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
 }
 console.log(getRandomComputerResult());
 
 function hasPlayerWonTheRound(player, computer) {
-    
+    return (
+        (player === "Rock" && computer === "Scissors") ||
+        (player === "Scissors" && computer === "Paper") ||
+        (player === "Paper" && computer === "Rock")
+      );
 }
+
 console.log(hasPlayerWonTheRound("Rock", "Scissors"));
 console.log(hasPlayerWonTheRound("Scissors", "Rock"));
